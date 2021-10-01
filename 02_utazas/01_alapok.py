6b8f74a8a0fd905d469969204ba5af22d5d093a8
gyzsombor6X <dangerx088@gmail.com>
2021-10-01 18:13:03 +0200

    Add files via upload

---
+++ 02_utazas/01_alapok.py
@@ -0,0 +1,11 @@
+print("Hello!")
+print("Add meg a neved!")
+nev=input()
+print("Szia, üdvözöllek "+nev)
+print("Adj meg egy számot: ")
+szam=int(input())
+25
+if szam>0:
+    print("Pozitív szám")
+else:
+    print("Nem pozitív szám")
