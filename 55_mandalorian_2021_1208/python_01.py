e1b68b4d55ccb74472583fb7001f51e692128cab
gyzsombor6X <dangerx088@gmail.com>
2022-01-19 12:48:39 +0100

    Delete 55_mandalorian_2021_1208 directory

--- 55_mandalorian_2021_1208/python_01.py
+++
@@ -1,9 +0,0 @@
-#szoveg=input()
-#kecske=szoveg.split(" ")
-print("Add meg a felső határt: ", end="")
-szam1=int(input()) #"""kecske[0]"""
-print("Add meg az alsó határt: ", end="")
-szam2=int(input()) #"""kecske[1]"""
-for i in range(szam1, szam2-1, -1):
-    print(i, end=" ")
-input()
--- 55_mandalorian_2021_1208/python_02.py
+++
@@ -1,6 +0,0 @@
-szoveg=input()
-kecske=szoveg.split(" ")
-n=int(kecske[0])
-c=kecske[1]
-import random
-for i in range()
